60fc99666eb1e9f65caaa097b35384a1f6cb3b32
YamYam <givsoul@gmail.com>
2022-06-30 14:01:47 +0200

    Added Tailwind CSS

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '../app.css';
+</script>
+
+<svelte:head>
+    <title>Svelte Chat</title>
+</svelte:head>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-	let appName = 'Svelte Chat';
+    const appName = 'Svelte Chat';
 </script>
 
-<h1>Welcome to {appName}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="m-10">
+    <h1 class="text-center text-pink-800 text-3xl">Welcome to {appName}</h1>
+    <p>
+        Visit <a
+            class="text-blue-500 hover:text-pink-700 transition-colors duration-500 text-lg"
+            href="https://kit.svelte.dev">kit.svelte.dev</a
+        > to read the documentation.
+    </p>
+</div>
